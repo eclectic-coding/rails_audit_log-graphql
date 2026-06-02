5f8b2b7ff2c8a1aaa502528fdedb2f545c085c60
Chuck Smith <eclectic-coding@users.noreply.github.com>
2026-06-02 18:25:36 -0400

    Copy base setup from another application

---
+++ lib/rails_audit_log/graphql.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "graphql/version"
+
+module RailsAuditLog
+  module Graphql
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rails_audit_log/graphql_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsAuditLog::Graphql do
+  it "has a version number" do
+    expect(RailsAuditLog::Graphql::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
